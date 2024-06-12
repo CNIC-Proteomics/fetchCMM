215518d7606b117a07e14a881e4e0c33b7211a9f
rbarreror <fitael@gmail.com>
2024-06-12 15:17:50 +0200

    fethCMM

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
+// Libraries
 const express = require('express')
+const cors = require('cors')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+// Constants
 const app = express()
 const port = process.env.PORT || 8000
+const CMM_URI = "http://ceumass.eps.uspceu.es/mediator/api/v3/batch";
 
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routing
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/get_cmm', async (req, res) => {
+
+    const resCMM = await new Promise(res => {
+        fetch(
+            CMM_URI,
+            {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(req.body)
+            }
+        ).then(value => res(value));
+    });
+    const resCMMJson = await resCMM.json();
+    res.json(resCMMJson);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
